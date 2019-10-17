bf45ad9d408244687c4ecf4eb53387dfe4177399
AravindNair430 <44204659+AravindNair430@users.noreply.github.com>
2019-10-17 09:15:54 +0530

    Update echoserver.java

--- basic server and client/echoserver.java
+++ basic server and client/echoserver.java
@@ -1,17 +1,16 @@
-
-// It is possible for more than one server to be running on a machine at any
-// given time.
-// When OS receives a request for service on a machine, it will also receive a
-// port number.
-// The port number will identify the server to where the request should be
-// forwarded.
-// A server is, thus, identified by its combination of ip address and port
-// number.
-// Typically, a client will issue a request to a server.
-// The server will receive a requestand send back a response.
-// The nature of request or response and the protocol used for communication is
-// dependent on client or server.
-// Sometimes a well documented protocol, such as HTTP is used.
+/* It is possible for more than one server to be running on a machine at any
+   given time.
+   When OS receives a request for service on a machine, it will also receive a
+   port number.
+   The port number will identify the server to where the request should be
+   forwarded.
+   A server is, thus, identified by its combination of ip address and port
+   number.
+   Typically, a client will issue a request to a server.
+   The server will receive a requestand send back a response.
+   The nature of request or response and the protocol used for communication is
+   dependent on client or server.
+   Sometimes a well documented protocol, such as HTTP is used.*/
 
 import java.io.*;
 import java.net.*;
